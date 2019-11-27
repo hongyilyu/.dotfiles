b2db5ea22c4a68b979726fce05d1f30201602e61
Hongyi Lyu <hongyilyu@hongyis-mbp.home>
2019-11-27 18:14:28 -0500

    vim plug

--- ubuntu_install_software.sh
+++ ubuntu_install_software.sh
@@ -9,6 +9,6 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install vim python3 python3-pip tmux neovim zsh nodejs npm curl autojump
 npm install -g neovim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pip3 install --system --upgrade mackup
