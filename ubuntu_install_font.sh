ac71bb235fa5116d4caa6a686a0c09ad10f9bd27
Hongyi Lyu <hongyilyu@hongyis-mbp.home>
2019-11-27 20:17:52 -0500

    switch order

--- ubuntu_install_font.sh
+++ ubuntu_install_font.sh
@@ -10,6 +10,5 @@ cd ~
 git clone https://github.com/LHY-iS-Learning/Sauce-Code-Pro
 mkdir -p ~/.fonts
 cp -v Sauce-Code-Pro/Regular/complete/*Complete.ttf ~/.fonts
-apt install fontconfig
 fc-cache -f
 rm -rf Sauce-Code-Pro
