8e11443e186ba47cbdbef9825e42f5f9d478e1aa
Hongyi Lyu <hongyilyu@hongyis-mbp.home>
2020-06-02 21:48:29 -0700

    delete ubuntu related scripts

--- ubuntu/ubuntu_restore_config.sh
+++
@@ -1,9 +0,0 @@
-cp $HOME/.dotfiles/.mackup.cfg $HOME/
-mackup restore
-source $HOME/.zshrc
-vimupdate
-
-gconftool-2 --dump '/apps/gnome-terminal' > /tmp/gnome-terminal-conf.xml
-gconftool-2 --load /tmp/gnome-terminal-conf.xml
-
-echo "22" | bash -c  "$(wget -qO- https://git.io/vQgMr)"
