731cba6ee2b4203fddab08b58ba87763caba186d
Hongyi Lyu <hongyilyu@hongyis-mbp.home>
2020-06-09 23:27:50 -0700

    hammerspoon add alacritty

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -21,6 +21,7 @@ end
 twoEvents = {hs.window.filter.windowFocused, hs.window.filter.windowCreated}
 set_app_input_method('Hammerspoon', English, hs.window.filter.windowCreated)
 set_app_input_method('iTerm2', English, twoEvents)
+set_app_input_method('Alacritty', English, twoEvents)
 set_app_input_method('Google Chrome', English, twoEvents)
 set_app_input_method('Firefox', English, twoEvents)
 set_app_input_method('Code', English, twoEvents)
