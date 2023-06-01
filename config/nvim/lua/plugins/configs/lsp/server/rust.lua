f14324148d0d289544d0cae80338f961259621cc
Hongyi Lyu <hongyi.lyu95@gmail.com>
2023-06-01 14:47:56 +0800

    AstroNvim like migration vol.7 - DAP, but not verified.

--- config/nvim/lua/plugins/configs/lsp/server/rust.lua
+++ config/nvim/lua/plugins/configs/lsp/server/rust.lua
@@ -1,3 +1,27 @@
+local adapter = function()
+  local success, package = pcall(function()
+    return require("mason-registry").get_package "codelldb"
+  end)
+  if success then
+    local package_path = package:get_install_path()
+    local codelldb_path = package_path .. "/codelldb"
+    local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+    local this_os = vim.loop.os_uname().sysname
+
+    -- The path in windows is different
+    if this_os:find "Windows" then
+      codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
+      liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+    else
+      -- The liblldb extension is .so for linux and .dylib for macOS
+      liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+    end
+    return require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+  else
+    return require("rust-tools.dap").get_codelldb_adapter()
+  end
+end
+
 return {
   tools = {
     executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
@@ -45,4 +69,7 @@ return {
       },
     },
   },
+  dap = {
+    adapter = adapter()
+  }
 }
