344a700bd3e6f62e00e61a78020a15f63efd6b3c
Hongyi Lyu <hongyi.lyu95@gmail.com>
2025-11-09 10:28:04 -0500

    refresh

--- config/nvim/after/ftplugin/dart.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.expandtab = true    -- Use spaces instead of tabs
-vim.opt.tabstop = 2         -- Number of spaces that a <Tab> counts for
-vim.opt.shiftwidth = 2      -- Number of spaces to use for autoindent
-vim.opt.softtabstop = 2     -- Number of spaces when pressing <Tab> or <BS>
-vim.opt.smartindent = true  -- Autoindent new lines
--- zsh/dev.zsh
+++ zsh/dev.zsh
@@ -1,7 +1,5 @@
-export PATH="$PATH:`yarn global bin`"
 export PATH="$PATH:/opt/homebrew/bin"
-export PATH="$PATH:$HOME/dev/flutter/bin"
-export PATH="$PATH:$HOME/dev/flutter2/bin"
+export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
 export PATH="$PATH:$HOME/Library/Python/3.9/bin"
 
@@ -9,3 +7,10 @@ export PATH="$PATH:$HOME/Library/Python/3.9/bin"
 autoload bashcompinit && bashcompinit
 autoload -Uz compinit && compinit
 complete -C '/usr/local/bin/aws_completer' aws
+
+# bun completions
+[ -s "/Users/hongyi/.bun/_bun" ] && source "/Users/hongyi/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
